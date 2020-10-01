f88e559d70103eb59c7494a75da99789d341452b
Robert Handi Wijaya <roberthandiwijaya@gmail.com>
2020-10-02 01:50:28 +0700

    Making UI for halaman_utama.dart

--- lib/halaman_utama.dart
+++ lib/halaman_utama.dart
@@ -12,21 +12,63 @@ class _HalamanUtamaState extends State<HalamanUtama> {
       appBar: AppBar(
         title: Text('Stock Calculator'),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
         children: [
-          Column(
-            children: [
-              Container(
-                color: Color(0xFF1D1E33),
-                child: Text(
-                  'Test',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                  ),
+          Expanded(
+            child: GestureDetector(
+              onTap: (){
+                setState(() {
+                  print('button 1 touched');
+                });
+              },
+              child: Container(
+                color: Colors.blueAccent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Center(
+                      child: Text(
+                        'Hitung Gross Profit Margin (GPM)',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
+            ),
+          ),
+          Divider(
+            color: Colors.white,
+            thickness: 4.0,
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: (){
+                print('button 2 touched');
+              },
+              child: Container(
+                color: Colors.blueAccent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Center(
+                      child: Text(
+                        'Hitung Return Of Equity (ROE)',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ class StockCalc extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Color(0xFF0A0E21),
+          scaffoldBackgroundColor: Color(0xFF0A0E21)
+      ),
         home: HalamanUtama(),
     );
   }
